e3c5636b71602cd9271071a759947218907ebfe2
Ani <anirudh2000@gmail.com>
2024-12-10 03:03:02 +0000

    intial commit

--- models/analytics/agg_installs_and_campaigns.sql
+++ models/analytics/agg_installs_and_campaigns.sql
@@ -1,8 +1,10 @@
-SELECT 
+SELECT
     -- install events data
     install_time,
     campaign_name,
     platform,
     COUNT(DISTINCT customer_id) AS distinct_installs
 FROM staging.app_installs_v2
-GROUP BY 1,2,3
+GROUP BY 1, campaign_name, 3
+
+-- change campaign_name to 2
